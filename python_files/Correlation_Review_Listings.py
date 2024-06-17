8d278483173d07e263247b33bd81512cb6aefe18
PC Tung Khuat <anhtung.khuat@stud.tu-darmstadt.de>
2024-06-17 16:07:02 +0200

    Update Correlation_Review_Listings.py

--- python_files/Correlation_Review_Listings.py
+++ python_files/Correlation_Review_Listings.py
@@ -10,6 +10,8 @@ Created on Mon Jun 17 15:10:12 2024
 Created on Mon Jun 17 10:23:13 2024
 
 @author: Tung
+
+Gilt der Voranalyse der Airbnb-Daten
 """
 import os
 import pandas as pd
@@ -44,25 +46,43 @@ Fragestellungen:
 df_rev = df_listings[df_listings['name'].str.contains(r'\d+\.\d+', regex=True)]
 df_no_rev = df_listings[~df_listings['name'].str.contains(r'\d+\.\d+', regex=True)]
 
-plt_0 = 0
+plt_0 = 1
 
 #%% Kommen die listings ohne Reviews auch in df_review vor? 
 
 df_no_rev['id_in_reviews'] = df_no_rev['id'].isin(df_review['listing_id'])
+df_rev['id_in_reviews'] = df_rev['id'].isin(df_review['listing_id'])
 
 # Zähle die Anzahl der True und False Werte
-count_id_in_rev = df_no_rev['id_in_reviews'].value_counts()
+count_id_in_rev_0 = df_no_rev['id_in_reviews'].value_counts()
+count_id_in_rev_1 = df_rev['id_in_reviews'].value_counts()
 if plt_0:
     # Diagramm erstellen
-    count_id_in_rev.plot(kind='bar')
-    plt.title(f'Analyse der Listings ohne Bewertung in "name" bei Gesamtanzahl von {len(df_no_rev)} Werten')
-    plt.xlabel('Listings ohne Bewertung in Namen kommt in "reviews.csv" vor')
+    count_id_in_rev_0.plot(kind='bar')
+    plt.title(f'Analyse der Listings OHNE Bewertung in "name" bei Gesamtanzahl von {len(df_no_rev)} Werten')
+    plt.xlabel('Listings OHNE Bewertung in Namen kommt in "reviews.csv" vor')
     plt.ylabel('Anzahl der IDs')
     plt.xticks(ticks=[0, 1], labels=['False', 'True'], rotation=0)
     plt.show()
     # Anzahl über jedem Balken anzeigen
-    for i in range(len(count_id_in_rev)):
-        plt.text(i, count_id_in_rev[i] + 0.1, str(count_id_in_rev[i]), ha='center', va='bottom')
+    for i in range(len(count_id_in_rev_0)):
+        plt.text(i, count_id_in_rev_0[i] + 0.1, str(count_id_in_rev_0[i]), ha='center', va='bottom')
+    
+    plt.figure()
+    count_id_in_rev_1.plot(kind='bar')
+    plt.title(f'Analyse der Listings MIT Bewertung in "name" bei Gesamtanzahl von {len(df_rev)} Werten')
+    plt.xlabel('Listings MIT Bewertung in Namen kommt in "reviews.csv" vor')
+    plt.ylabel('Anzahl der IDs')
+    plt.xticks(ticks=[0, 1], labels=['True', 'False'], rotation=0)
+    plt.show()
+    # Anzahl über jedem Balken anzeigen
+    for i in range(len(count_id_in_rev_1)):
+        plt.text(i, count_id_in_rev_1[i] + 0.1, str(count_id_in_rev_1[i]), ha='center', va='bottom')
+        
+    
+        
+        
+        
     
 #%% Wie viele Reviews hat so ein Listing im Schnitt, was ist Maximum und Minimum?
 
